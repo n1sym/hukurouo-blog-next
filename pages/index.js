001088a19eb13f0126ff0b753f551d41cd511f42
hukurouo <owlbook248@gmail.com>
2021-01-14 23:32:56 +0900

    Dynamic Routes まで

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,40 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
+      {/* Keep the existing code here */}
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>
+    <a>{title}</a>
+  </Link>
+  <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
---
+++ posts/ssg-ssr.md
@@ -0,0 +1,10 @@
+---
+title: 'When to Use Static Generation v.s. Server-side Rendering'
+date: '2020-01-02'
+---
+
+<figure><img src="https://firebasestorage.googleapis.com/v0/b/hukurouo.appspot.com/o/images%2FEqdif8iU8AAD7tc.jpg?alt=media&token=b2994623-9e97-4d02-8a24-74283fbe3f11"><figcaption>こうして見るとアフタヌーン作品が多い</figcaption></figure>
+
+
+
+test
